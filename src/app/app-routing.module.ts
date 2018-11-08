a92a746e2b740836bbb6fb7416c990bafb3d4f6e
baofree <baofree.wook@gmail.com>
2018-11-08 09:39:26 +0900

    box model, grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {W3CssDropdownsComponent} from './w3schools/dropdowns/w3-css-dropdowns.component';
+import {BoxModelComponent} from './basic/box-model/box-model.component';
+import {GridComponent} from './basic/grid/grid.component';
 
 const routes: Routes = [
   {
     path: 'w3schools',
     children: [
       {path: 'dropdowns', component: W3CssDropdownsComponent}
+    ]
+  },
+  {
+    path: 'basics',
+    children: [
+      {path: 'box-model', component: BoxModelComponent},
+      {path: 'grid', component: GridComponent}
     ]
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { W3CssDropdownsComponent } from './w3schools/dropdowns/w3-css-dropdowns.component';
+import { BoxModelComponent } from './basic/box-model/box-model.component';
+import { GridComponent } from './basic/grid/grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    W3CssDropdownsComponent
+    W3CssDropdownsComponent,
+    BoxModelComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
